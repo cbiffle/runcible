398733bd05b29da49fb94b0acf94a518e608e466
Cliff L. Biffle <cbiffle+github@gmail.com>
2009-03-28 11:56:04 -0700

    PDF TOC is now hierarchical.

--- pdf-component/toc/tocman.cpp
+++ pdf-component/toc/tocman.cpp
@@ -8,14 +8,40 @@ TocManager::TocManager(const QString &path, QObject *parent)
 
 TocManager::~TocManager() { }
 
+static void copyToc(const QUrl &docUrl, Poppler::Document *doc, QDomNode node,
+    QList<Choice> &choices, int indent) {
+  while (!node.isNull()) {
+    if (node.isElement()) {
+      QDomElement el = node.toElement();
+      QString destName = el.attribute("DestinationName", QString());
+      if (!destName.isNull()) {
+        Poppler::LinkDestination *dest = doc->linkDestination(destName);
+        QUrl refUrl(docUrl);
+        refUrl.setFragment("page:" + QString::number(dest->pageNumber()));
+        delete dest;
+       
+        QString displayName(QString(indent * 2, ' ') + el.nodeName());
+        choices << Choice(displayName, refUrl.toString()); 
+      }
+    }
+    if (node.hasChildNodes()) {
+      copyToc(docUrl, doc, node.firstChild(), choices, indent + 1);
+    }
+    node = node.nextSibling();
+  }
+}
+
 void TocManager::refresh() {
   Poppler::Document *doc = Poppler::Document::load(_path);
 
   if (doc) {
     QList<Choice> choices;
+    QUrl docUrl("file:" + _path);
     QDomDocument *toc = doc->toc();
     if (toc) {
       QDomNode child = toc->documentElement();
+      copyToc(docUrl, doc, child, choices, 0);
+      /*
       while (!child.isNull()) {
         if (child.isElement()) {
           QDomElement el = child.toElement();
@@ -27,6 +53,7 @@ void TocManager::refresh() {
         }
         child = child.nextSibling();
       }
+      */
       delete toc;
     } else {
       qDebug() << "NO TOC";
@@ -39,5 +66,6 @@ void TocManager::refresh() {
 }
 
 void TocManager::activate(Choice c) {
+  qDebug() << c.id();
   _coordinator.openItem("runcible-view-pdf", QUrl(c.id()));
 }
